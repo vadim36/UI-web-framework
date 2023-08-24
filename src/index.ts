b7d7c72e2dce8d4c393adb5a540c28b8c3479e5b
Vadim Medvedev <vadim130809@gmail.com>
2023-08-24 09:40:36 +0300

    Fix bugs

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import './styles/_index.scss';
+import { ElementSize } from "./abstract/Abstract";
+import { WaveButton } from "./components/WaveButton";
+import { MenuHamburger } from './components/MenuBurger';
+//import { Tabs } from './components/Tabs';
+
+export default {
+  ElementSize,
+  WaveButton,
+  MenuHamburger,
+  //Tabs
+}
